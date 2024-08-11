1a0dbd50ab22500604b32d15bd766bcc7346f0bc
MonikaSivakumar2004 <monikasivakumar240@gmail.com>
2024-08-11 10:33:41 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+var ulist=document.getElementById("list");
+var input=document.getElementById("input");
+function addEventListener(){
+    let cht=document.createElement("live");
+    live.innerHTML=input.Value;
+    ul.append("live");
+
+}
